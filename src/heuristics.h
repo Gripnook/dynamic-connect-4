1194f42ee1acba957c613e518e0f00a2950198bd
Gripnook <xxandreixx@hotmail.com>
2017-09-15 21:45:10 -0400

    Optimized heuristic computation

--- src/heuristics.h
+++ src/heuristics.h
@@ -200,27 +200,36 @@ private:
         eval(int player, const StateType& state, const Drawboard& board) const
     {
         EvalType result = 0;
+        // Since the pieces are sorted, we only need to look forward in the row,
+        // column, diagonal, and antidiagonal for each piece.
         auto& pieces = player == 1 ? state.whitePieces : state.blackPieces;
-        for (auto it1 = std::begin(pieces); it1 != std::end(pieces); ++it1)
+        for (const auto& piece : pieces)
         {
-            for (auto it2 = it1 + 1; it2 != std::end(pieces); ++it2)
-            {
-                auto dx = std::abs(it2->x() - it1->x());
-                auto dy = std::abs(it2->y() - it1->y());
-                auto maxDistance = std::max(dx, dy);
-                if (maxDistance == 1)
-                {
-                    result += 1.0f;
-                }
-                else if (maxDistance == 2 && (dx % 2 == 0) && (dy % 2 == 0))
-                {
-                    auto mx = (it2->x() + it1->x()) / 2;
-                    auto my = (it2->y() + it1->y()) / 2;
-                    auto value = board.get(mx, my);
-                    if (value == player)
-                        result += 2.0f;
-                }
-            }
+            auto x = piece.x();
+            auto y = piece.y();
+            int count = 0;
+            for (int i = 1; board.get(x + i, y - i) == player; ++i)
+                ++count;
+            if (count > 0)
+                result += 2 * count - 1;
+
+            count = 0;
+            for (int i = 1; board.get(x + i, y) == player; ++i)
+                ++count;
+            if (count > 0)
+                result += 2 * count - 1;
+
+            count = 0;
+            for (int i = 1; board.get(x + i, y + i) == player; ++i)
+                ++count;
+            if (count > 0)
+                result += 2 * count - 1;
+
+            count = 0;
+            for (int i = 1; board.get(x, y + i) == player; ++i)
+                ++count;
+            if (count > 0)
+                result += 2 * count - 1;
         }
         return result;
     }
