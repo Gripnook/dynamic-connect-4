7f4ae3c500ec9f272bf08a9b5d5d4c3790a806af
Gripnook <xxandreixx@hotmail.com>
2017-09-21 05:26:11 -0400

    Revert "Added depth based transposition table"
    This reverts commit 7fab93d33d3502843f8c5b9d79135bc2ed55de51.

--- src/iterative-alpha-beta.h
+++ src/iterative-alpha-beta.h
@@ -33,8 +33,6 @@ public:
         this->heuristic = heuristic;
         startTime = std::chrono::high_resolution_clock::now();
 
-        transpositionTable.expire();
-
         auto actions = game.getActions(state);
         std::map<ActionType, EvalType> values;
 
--- src/transposition-table.h
+++ src/transposition-table.h
@@ -3,7 +3,6 @@
 #include <list>
 #include <unordered_map>
 #include <utility>
-#include <map>
 
 namespace Search {
 
@@ -36,23 +35,16 @@ public:
 
     using ListType = std::list<std::pair<StateType, ValueType>>;
     using MapType = std::unordered_map<StateType, typename ListType::iterator>;
-    using DepthIteratorMapType = std::map<int, typename ListType::iterator>;
 
-    static const size_t lruMaxSize = 2 * 1024 * 1024;
-    static const size_t depthMaxSize = 2 * 1024 * 1024;
+    static const size_t maxSize = 4 * 1024 * 1024;
 
     std::pair<bool, ValueType> find(const StateType& state)
     {
         ++accesses;
-        auto entry = lruTable.find(state);
-        if (entry != std::end(lruTable))
+        auto entry = table.find(state);
+        if (entry != std::end(table))
         {
-            lruList.splice(std::begin(lruList), lruList, entry->second);
-            return std::make_pair(true, entry->second->second);
-        }
-        entry = depthTable.find(state);
-        if (entry != std::end(depthTable))
-        {
+            lru.splice(std::begin(lru), lru, entry->second);
             return std::make_pair(true, entry->second->second);
         }
         ++misses;
@@ -61,27 +53,33 @@ public:
 
     void emplace(const StateType& state, EvalType value, int depth, Flag flag)
     {
-        lruEmplace(state, value, depth, flag);
-        depthEmplace(state, value, depth, flag);
-    }
-
-    void expire()
-    {
-        depthExpire();
+        auto entry = table.find(state);
+        if (entry != std::end(table))
+        {
+            lru.splice(std::begin(lru), lru, entry->second);
+            entry->second->second = ValueType{value, depth, flag};
+        }
+        else
+        {
+            lru.emplace_front(state, ValueType{value, depth, flag});
+            table[state] = std::begin(lru);
+            while (size() > maxSize)
+            {
+                table.erase(lru.back().first);
+                lru.pop_back();
+            }
+        }
     }
 
     void clear()
     {
-        lruTable.clear();
-        lruList.clear();
-        depthTable.clear();
-        depthList.clear();
-        depthIterators.clear();
+        table.clear();
+        lru.clear();
     }
 
     size_t size() const
     {
-        return lruTable.size() + depthTable.size();
+        return table.size();
     }
 
     double getHitRate() const
@@ -91,93 +89,10 @@ public:
     }
 
 private:
-    MapType lruTable;
-    ListType lruList;
-
-    MapType depthTable;
-    ListType depthList;
-    DepthIteratorMapType depthIterators;
+    MapType table;
+    ListType lru;
 
     mutable int accesses{0};
     mutable int misses{0};
-
-    void lruEmplace(const StateType& state, EvalType value, int depth, Flag flag)
-    {
-        auto entry = lruTable.find(state);
-        if (entry != std::end(lruTable))
-        {
-            lruList.splice(std::begin(lruList), lruList, entry->second);
-            entry->second->second = ValueType{value, depth, flag};
-        }
-        else
-        {
-            lruList.emplace_front(state, ValueType{value, depth, flag});
-            lruTable[state] = std::begin(lruList);
-            while (lruTable.size() > lruMaxSize)
-            {
-                lruTable.erase(lruList.back().first);
-                lruList.pop_back();
-            }
-        }
-    }
-
-    void
-        depthEmplace(const StateType& state, EvalType value, int depth, Flag flag)
-    {
-        auto entry = depthTable.find(state);
-        if (entry != std::end(depthTable))
-        {
-            entry->second->second = ValueType{value, depth, flag};
-            depthMaintain(depth, entry->second);
-        }
-        else
-        {
-            depthList.emplace_back(state, ValueType{value, depth, flag});
-            depthTable[state] = --std::end(depthList);
-            depthMaintain(depth, --std::end(depthList));
-            while (depthTable.size() > depthMaxSize)
-            {
-                for (auto& it : depthIterators)
-                {
-                    if (it.second != std::begin(depthList))
-                        break;
-                    ++it.second;
-                }
-                depthTable.erase(depthList.front().first);
-                depthList.pop_front();
-            }
-        }
-    }
-
-    void depthMaintain(int depth, typename ListType::iterator listIt)
-    {
-        auto it = depthIterators.find(depth + 1);
-        if (it != std::end(depthIterators))
-        {
-            depthList.splice(it->second, depthList, listIt);
-        }
-        else
-        {
-            depthList.splice(std::end(depthList), depthList, listIt);
-            it = depthIterators.find(depth);
-            if (it == std::end(depthIterators))
-                depthIterators.emplace(depth, --std::end(depthList));
-        }
-    }
-
-    void depthExpire()
-    {
-        if (std::begin(depthIterators) == std::end(depthIterators))
-            return;
-        auto it1 = std::begin(depthIterators);
-        auto it2 = ++std::begin(depthIterators);
-        while (it2 != std::end(depthIterators))
-        {
-            it2->second = it1->second;
-            ++it1;
-            ++it2;
-        }
-        depthIterators.erase(--std::end(depthIterators));
-    }
 };
 }
