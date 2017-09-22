3f9764cea2ef4136112066c97ef474a13e36f8ee
Gripnook <XxAndreixX@hotmail.com>
2017-09-21 23:32:15 -0400

    Added includes

--- src/gclient.h
+++ src/gclient.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <limits>
 #include <thread>
 
 #include "game/game.h"
--- src/search/iterative-alpha-beta.h
+++ src/search/iterative-alpha-beta.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <vector>
 #include <limits>
 #include <algorithm>
 #include <utility>
