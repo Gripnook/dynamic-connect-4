81f155955616308aba1db9fe89273fe8d1435ead
Gripnook <XxAndreixX@hotmail.com>
2017-09-21 22:46:12 -0400

    Improved minimax

--- src/search/minimax.h
+++ src/search/minimax.h
@@ -7,6 +7,25 @@
 
 namespace Search {
 
+// A class implementing the minimax search algorithm for a game of type Game.
+//
+// Game must define:
+//      StateType - The type of the state representation for a position.
+//      ActionType - The type of an action in the game.
+//      EvalType - The type of a numerical position evaluation.
+//
+//      std::vector<ActionType> getActions(StateType)
+//          A method to get a vector with the possible actions
+//          that can be taken from a given state.
+//
+//      StateType getResult(StateType, ActionType)
+//          A method to apply an action to a state and get the next state.
+//
+//      bool isTerminal(StateType)
+//          A method to check if a state is terminal.
+//
+//      EvalType getUtility(StateType)
+//          A method to get the utility value of a given terminal state.
 template <typename Game>
 class Minimax
 {
@@ -17,11 +36,12 @@ public:
 
     using Heuristic = std::function<EvalType(const StateType&)>;
 
-    Minimax(Game& game, int maxDepth) : game(game), maxDepth{maxDepth}
+    Minimax(Game& game) : game(game)
     {
     }
 
-    ActionType search(const StateType& state, Heuristic heuristic, bool isMax)
+    ActionType
+        search(const StateType& state, Heuristic heuristic, int depth, bool isMax)
     {
         count = 1;
         this->heuristic = heuristic;
@@ -38,10 +58,12 @@ public:
         auto bestAction = std::make_pair(actions.front(), init);
         for (const auto& action : actions)
         {
-            auto value = minimax(game.getResult(state, action), 1, !isMax);
+            auto value =
+                minimax(game.getResult(state, action), depth - 1, !isMax);
             if (comp(value, bestAction.second))
                 bestAction = std::make_pair(action, value);
         }
+        this->depth = depth;
         return bestAction.first;
     }
 
@@ -52,12 +74,12 @@ public:
 
     int getLastDepth() const
     {
-        return maxDepth;
+        return depth;
     }
 
 private:
     Game& game;
-    int maxDepth{};
+    int depth{};
     int count{0};
     Heuristic heuristic;
 
@@ -66,7 +88,7 @@ private:
         ++count;
         if (game.isTerminal(state))
             return game.getUtility(state);
-        else if (depth >= maxDepth)
+        else if (depth == 0)
             return heuristic(state);
 
         auto init = isMax ? std::numeric_limits<EvalType>::lowest() :
@@ -81,7 +103,7 @@ private:
         for (const auto& action : game.getActions(state))
         {
             auto value =
-                minimax(game.getResult(state, action), depth + 1, !isMax);
+                minimax(game.getResult(state, action), depth - 1, !isMax);
             if (comp(value, bestValue))
                 bestValue = value;
         }
